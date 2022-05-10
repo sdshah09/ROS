97226bef9a22f39138447c847889ccbee0a8d7fe
sdshah09 <99328539+sdshah09@users.noreply.github.com>
2022-05-10 10:25:47 +0530

    Sophus Build Files

---
+++ ORB_SLAM3/Thirdparty/Sophus/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/shaswat/Desktop/ORB_SLAM3-master/Thirdparty/Sophus
+# Build directory: /home/shaswat/Desktop/ORB_SLAM3-master/Thirdparty/Sophus/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("test")
+subdirs("examples")
