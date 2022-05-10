1c7c21e4da2f50e1362e3fc556059ef3f2c03c12
sdshah09 <99328539+sdshah09@users.noreply.github.com>
2022-05-10 10:28:33 +0530

    Add files via upload

---
+++ ORB_SLAM3/Thirdparty/Sophus/build/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/shaswat/Desktop/ORB_SLAM3-master/Thirdparty/Sophus/test
+# Build directory: /home/shaswat/Desktop/ORB_SLAM3-master/Thirdparty/Sophus/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("core")
+subdirs("ceres")
